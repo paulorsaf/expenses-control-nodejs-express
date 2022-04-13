c7104f7fc1c7dc36126f64381e6138bb19ee2135
Paulo Alves <paulorsaf@gmail.com>
2022-04-13 18:08:48 -0300

    Conectando a aplicaçao nodejs + express ao firestore

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import admin from 'firebase-admin';
 
-// REST API http://api.controle-de-gastos.com/transactions
 const app = express();
 
-// GET      http://api.controle-de-gastos.com/transactions
+admin.initializeApp({
+  credential: admin.credential.cert("serviceAccountKey.json")
+});
+
 app.get('/transactions', (request, response) => {
-    console.log('GET transactions');
-    response.json([{id: 1}]);
+    admin.firestore()
+        .collection('transactions')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.id
+            }))
+            response.json(transactions);
+        })
 })
 
-app.listen(3000, () => console.log('API rest iniciada em http://localhost:3000'))
+app.listen(3000, () => console.log('API rest iniciada em http://localhost:3000'));
