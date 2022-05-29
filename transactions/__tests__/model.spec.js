c341a3501d6b49798e6455ccdc03ddb75053c563
Paulo Alves <paulorsaf@gmail.com>
2022-05-29 13:30:28 -0300

    Refactoring tests

--- transactions/__tests__/model.spec.js
+++ transactions/__tests__/model.spec.js
@@ -132,44 +132,6 @@ describe("Transaction model", () => {
     })
 
     describe('given update transaction', () => {
-        
-        let repositoryMock;
-
-        beforeEach(() => {
-            repositoryMock = {
-                _hasUpdated: false,
-                findByUid() {
-                    return Promise.resolve({user: {uid: "anyUserUid"}});
-                },
-                update() {
-                    this._hasUpdated = true;
-                    return Promise.resolve();
-                }
-            }
-        })
-
-        test('then return updated transaction', async () => {
-            const model = new Transaction(repositoryMock);
-            model.uid = 1;
-            model.user = {uid: "anyUserUid"};
-
-            const params = createTransaction();
-            await model.update(params);
-
-            const updatedTransaction = createTransaction();
-            expect(model).toEqual(updatedTransaction);
-        })
-
-        test('then update transaction', async () => {
-            const model = new Transaction(repositoryMock);
-            model.uid = 1;
-            model.user = {uid: "anyUserUid"};
-
-            const params = createTransaction();
-            await model.update(params);
-
-            expect(repositoryMock._hasUpdated).toBeTruthy();
-        })
 
         test('when transaction doesnt belong to user, then return error', async () => {
             const model = new Transaction({
@@ -193,6 +155,45 @@ describe("Transaction model", () => {
             const params = createTransaction();
             await expect(model.update(params))
                 .rejects.toBeInstanceOf(TransactionNotFoundError);
+        })
+
+        describe('when success', () => {
+        
+            let repositoryMock;
+            let model;
+    
+            beforeEach(() => {
+                repositoryMock = {
+                    _hasUpdated: false,
+                    findByUid() {
+                        return Promise.resolve({user: {uid: "anyUserUid"}});
+                    },
+                    update() {
+                        this._hasUpdated = true;
+                        return Promise.resolve();
+                    }
+                }
+
+                model = new Transaction(repositoryMock);
+                model.uid = 1;
+                model.user = {uid: "anyUserUid"};
+            })
+    
+            test('then return updated transaction', async () => {
+                const params = createTransaction();
+                await model.update(params);
+    
+                const updatedTransaction = createTransaction();
+                expect(model).toEqual(updatedTransaction);
+            })
+    
+            test('then update transaction', async () => {
+                const params = createTransaction();
+                await model.update(params);
+    
+                expect(repositoryMock._hasUpdated).toBeTruthy();
+            })
+
         })
 
     })
